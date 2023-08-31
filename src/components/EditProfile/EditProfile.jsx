22f32219588bd08b40ea98715c3df628adab7f2c
Halim <mohamedabdelhaleam13@gmail.com>
2023-08-31 14:21:53 +0300

    Responsive

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const EditProfile = () => {
   return (
     <div className='w-full 
-    xs:text-center
-    sm:text-center
-    md:text-center
-    lg:text-center
+        xs:text-center
+        sm:text-center
+        md:text-center
+        lg:text-center
     '>
         <div className='text-4xl font-bold'>Your Account</div>
         <div className='text-xl font-semibold'>Personal Information</div>
         <div>
             <form action="" className='mx-1'>
                 <div className='flex items-start gap-4 '>
-                    <div className='flex flex-col w-1/3 xs:w-1/2'>
+                    <div className='flex flex-col w-1/3 xs:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2'>
                         <br />
                         <label htmlFor="" className="text-xl font-medium">User Name*</label>
                         <input type="text" placeholder='Username' className="border-2 border-[#AAAAAA] px-2 py-2 placeholder:text-lg"/>
@@ -27,7 +27,7 @@ const EditProfile = () => {
                         <label htmlFor="" className="text-xl font-medium">Location*</label>
                         <input type="text" placeholder='Country' className="border-2 border-[#AAAAAA] px-2 py-2 placeholder:text-lg"/>
                     </div>
-                    <div className='flex flex-col w-1/3 xs:w-1/2'>
+                    <div className='flex flex-col w-1/3 xs:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2'>
                         <br />
                         <label htmlFor="" className="text-xl font-medium">Email*</label>
                         <input type="text" placeholder='Email' className="border-2 border-[#AAAAAA] px-2 py-2 placeholder:text-lg"/>
