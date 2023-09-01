603f25987e1c7074a345f5c0e925b415f71544d1
Halim <mohamedabdelhaleam13@gmail.com>
2023-09-01 11:54:19 +0300

    handle input in forget password

--- src/ForgetPassword/ForgetPassword.jsx
+++ src/ForgetPassword/ForgetPassword.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import logo from '../assets/images/YLE-logo.png'
 import ForgetImg from '../assets/images/Key-rafiki1.png'
 
 const ForgetPassword = () => {
+  const [email,setEmail] = useState("")
+  
+  const sendEmail =(e)=>{
+
+  }
   return (
     <div className='bg-[#FDF8EE] w-full h-screen py-6'>
       <div className='m-auto max-w-[1280px]'>
@@ -20,7 +25,10 @@ const ForgetPassword = () => {
                     <br />
                     <label htmlFor="" className='font-semibold text-2xl'>Email</label>
                     <br />
-                    <input type="email" className="px-8 my-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1] w-[70%]" placeholder='Type Your Email'/>
+                    <input type="email" className="px-8 my-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1] w-[70%]" placeholder='Type Your Email'
+                    onChange={(e)=>{
+                      setEmail(e.target.value)
+                    }}/>
                     <br /><br />
                     <div className='bg-[#2B2E7F] shadow-md text-white w-[70%] rounded-full flex justify-center items-center py-4 text-2xl font-bold px-4'>
                         <button>Send OTP</button>
