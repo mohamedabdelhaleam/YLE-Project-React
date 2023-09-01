b72da6199924b58c84a5110973f2930ff9e8790e
Halim <mohamedabdelhaleam13@gmail.com>
2023-09-01 11:31:35 +0300

    Add Some Logic to Login Page

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import logo from '../assets/images/YLE-logo.png'
 import loginImg from '../assets/images/Exlorer_Illustration1.png'
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [userName , setUserName] = useState("")
+    const [password , setPassword] = useState("")
+
+
+    console.log(userName , password);
+
+
+    const handleLogin =(e)=>{
+        
+    }
   return (
     <div className='bg-[#FDF8EE] w-full py-6'>
       <div className='m-auto max-w-[1280px]'>
@@ -22,17 +33,22 @@ const Login = () => {
                 <form action="" className='w-full xs:text-center sm:text-center md:text-center'>
                     <label htmlFor="" className='font-bold text-4xl'>WELCOME BACK!</label>
                     <br />
-                    <label htmlFor="" className="text-lg font-normal font-['Poppins'] ">Don’t have a account, <span>Sign up</span> </label>
+                    <label htmlFor="" className="text-lg font-normal font-['Poppins'] ">Don’t have a account, <span><Link to="/en/signup">Sign up</Link> </span> </label>
                     <br />
                     <br />
                     <label htmlFor="" className='font-semibold text-2xl'>Username</label>
                     <br />
-                    <input type="text" className="px-8 my-2 xs:py-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1] w-[70%] xs:w-[90%] sm:w-[90%]" placeholder='halim@gmail.com'/>
-                    <br />
+                    <input type="text" className="px-8 my-2 xs:py-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1] w-[70%] xs:w-[90%] sm:w-[90%]" placeholder='halim@gmail.com'
+                    onChange = {(e)=>{
+                        setUserName(e.target.value)
+                    }} />
                     <br />
                     <label htmlFor="" className='font-semibold text-2xl'>Password</label>
                     <br />
-                    <input type="password"  className="px-8  my-2 xs:py-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1]  w-[70%] xs:w-[90%] sm:w-[90%]" placeholder='halim@gmail.com'/>
+                    <input type="password"  className="px-8  my-2 xs:py-2 py-3 text-[#6C70D1] text-lg rounded-full border-2 border-[#6C70D1]  w-[70%] xs:w-[90%] sm:w-[90%]" placeholder='halim@gmail.com' 
+                    onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }}/>
                     <br />
                     <br />
                     <div className='flex justify-between items-center w-[70%] xs:w-[90%] xs:m-auto sm:w-[90%] sm:m-auto md:m-auto'>
@@ -44,7 +60,7 @@ const Login = () => {
                     </div>
                     <br />
                     <div className='bg-[#2B2E7F] shadow-md text-white w-[70%] xs:m-auto sm:m-auto md:m-auto rounded-full flex justify-center items-center py-4  text-2xl font-bold px-4'>
-                        <button>Sign In</button>
+                        <button >Sign In</button>
                     </div>
                     <br /><br />
                     <hr className='w-[70%] hr-text m-auto'  data-content="or continue with"/>
