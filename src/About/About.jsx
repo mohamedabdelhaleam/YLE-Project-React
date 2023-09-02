54befc5ffb4e09e2cb21dec2744cb863428ca3ba
Halim <mohamedabdelhaleam13@gmail.com>
2023-09-02 17:53:37 +0300

    Update Navbar routes

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -22,11 +22,11 @@ const About = () => {
                 </div>
                 <div className='flex justify-between items-center gap-12'>
                     <ul className='flex justify-between items-center gap-y gap-12'>
-                        <li><NavLink to='/' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>Home</NavLink></li>
-                        <li><NavLink to='/about' className='active:text-[#FF7426] text-[#FF7426] text-lg font-semibold capitalize'>About</NavLink></li>
-                        <li><NavLink to='/courses' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>courses</NavLink></li>
-                        <li><NavLink to='/our-service' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>our service</NavLink></li>
-                        <li><NavLink to='/contact-us' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>contact us</NavLink></li>
+                        <li><NavLink to='/en/' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>Home</NavLink></li>
+                        <li><NavLink to='/en/about' className='active:text-[#FF7426] text-[#FF7426] text-lg font-semibold capitalize'>About</NavLink></li>
+                        <li><NavLink to='/en/courses' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>courses</NavLink></li>
+                        <li><NavLink to='/en/our-service' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>our service</NavLink></li>
+                        <li><NavLink to='/en/contact-us' className='active:text-[#FF7426] text-[#1D1D1D] text-lg font-semibold capitalize'>contact us</NavLink></li>
                     </ul>
                     <div className='text-2xl font-medium capitalize bg-[#4D2C5E] rounded-full text-white py-2 px-8'>
                         login
