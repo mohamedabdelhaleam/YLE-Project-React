8d3c2dd4ab4af322e4676cd58036a6d09d019f3e
Halim <mohamedabdelhaleam13@gmail.com>
2023-09-02 12:57:32 +0300

    Create Arabic Components

--- src/ArabicComponents/ArAboutCard/ArAboutCard.jsx
+++ src/ArabicComponents/ArAboutCard/ArAboutCard.jsx
@@ -8,12 +8,10 @@ const ArAboutCard = () => {
       <div className='w-3/4 h-3 bg-[#C4C4C4] absolute bottom-[-6px]'></div>
       <div><MdOutlineRocketLaunch color='red' size={50}/></div>
       <div className='flex justify-center items-center flex-col gap-16 xs:gap-12'>
-        <div className="font-['Roboto'] text-2xl font-normal">Lorem Ipsum is simply</div>
-        <div className="font-['Roboto'] text-xl font-light">Lorem Ipsum is simply dummy
-          text of the printing and typesetting
-          industry. Lorem Ipsum has been
-          the industry's standard dummy
-          text ever since the 1500s,</div>
+        <div className="font-['Roboto'] text-2xl font-normal">لوريم إيبسوم بكل بساطة</div>
+        <div className="font-['Roboto'] text-xl font-light">لوريم إيبسوم مجرد دمية نص للطباعة والتنضيد صناعة. 
+لقد كان لوريم إيبسوم الدمية القياسية للصناعة النص 
+منذ القرن الخامس عشر الميلادي ،</div>
       </div>
     </div>
   );
