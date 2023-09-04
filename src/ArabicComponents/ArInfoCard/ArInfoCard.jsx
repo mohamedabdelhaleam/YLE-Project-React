a7a38b1eb1588cc2f93a674cf68d853a2073fc7f
Halim <mohamedabdelhaleam13@gmail.com>
2023-09-04 11:46:06 +0300

    Create Arabic Component

--- src/ArabicComponents/ArInfoCard/ArInfoCard.jsx
+++ src/ArabicComponents/ArInfoCard/ArInfoCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import quizCard from '../../assets/images/online-test1.png'
 
-const InfoCard = () => {
+const ArInfoCard = () => {
   return (
-    <div className='text-white flex justify-evenly items-center gap-3'>
+    <div className='text-white flex justify-evenly items-center flex-row-reverse gap-3'>
       <div>
         <div className='h-24 w-24 bg-white rounded-md flex justify-center items-center bg-opacity-20'>
           <img src={quizCard} alt="" />
         </div>
       </div>
-      <div>
+      <div className='text-right'>
         <div className='text-2xl'>احصل على شهادة</div>
         <div className='text-sm opacity-60'>خلافًا للاعتقاد الشائع ، فإن Lorem Ipsum ليس مجرد نص عشوائي. لها جذور في BC ، مما يجعلها أكثر من 2000 عام.</div>
       </div>
@@ -17,4 +17,4 @@ const InfoCard = () => {
   );
 }
 
-export default InfoCard;
+export default ArInfoCard;
